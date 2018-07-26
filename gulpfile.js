2bef3ddb56cdf7b9746a06b182ba4a4092748327
Grigoriy Levanov <glevanov@gmail.com>
2018-07-26 08:17:15 +0300

    Implements image minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('images', function(){
+  return gulp.src(['app/**/*.+(png|jpg|gif|svg)', '!app/guide/preview/*'])
+    .pipe(imagemin())
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('img'))
+});
